f064fa68335dfc3597626a4043cba05b6384143a
armanr99 <arman.rostami.999@gmail.com>
2020-07-21 18:30:38 +0430

    Add Tools class

--- P2PHost.py
+++ P2PHost.py
@@ -1,10 +1,9 @@
 import threading
 import time
 import config
-import socket
-import pickle
 
 from P2PPacket import P2PPacket
+from Tools import Tools
 
 class P2PHost:
     def __init__(self, host_address):
@@ -15,6 +14,7 @@ class P2PHost:
         self.unidirectional_sent_host_addresses = set()
         self.hosts_last_receive_time = dict()
         self.init_hosts_last_receive_time()
+        self.tools = Tools()
 
     def init_hosts_last_receive_time(self):
         for host_address in config.HOST_ADDRESSES:
@@ -27,14 +27,9 @@ class P2PHost:
         while not self.is_finished:
             for bidirectional_host_address in self.bidirectional_host_addresses:
                 hello_packet = self.get_hello_packet(bidirectional_host_address)
-                self.send_udp_packet(bidirectional_host_address, hello_packet)
+                self.tools.send_udp_packet(bidirectional_host_address, hello_packet)
             time.sleep(config.SEND_TIME)
 
     def get_hello_packet(self, host_address):
         return P2PPacket(self.host_address, config.HELLO_MESSAGE_TYPE, 
             self.bidirectional_host_addresses, time.time(), self.hosts_last_receive_time[host_address])
-
-    def send_udp_packet(self, host_address, data):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.sendto(pickle.dumps(data), (host_address[0], host_address[1]))
-        sock.close()
