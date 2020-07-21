7bef6eb1724b2a84c3cc1c5dfa0a5671b2bfb3c0
armanr99 <arman.rostami.999@gmail.com>
2020-07-21 17:07:34 +0430

    Add P2PPacket constructor

--- P2PPacket.py
+++ P2PPacket.py
@@ -1,2 +1,8 @@
 class P2PPacket:
-    pass
+    def __init__(self, host_ip, host_port, message_type, bidrectional_ports, last_time_sent, last_time_received):
+        self.host_ip = host_ip
+        self.host_port = host_port
+        self.message_type = message_type
+        self.bidrectional_ports = bidrectional_ports
+        self.last_time_sent = last_time_sent
+        self.last_time_received = last_time_received
