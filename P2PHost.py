640e0e62867c048440124037d517260b52c9c948
armanr99 <arman.rostami.999@gmail.com>
2020-07-21 19:33:05 +0430

    Add find neighbours

--- P2PHost.py
+++ P2PHost.py
@@ -1,5 +1,6 @@
 import threading
 import time
+import random
 import config
 
 from P2PPacket import P2PPacket
@@ -21,15 +22,44 @@ class P2PHost:
             self.hosts_last_receive_time[host_address] = config.UNDEFINED
             
     def start(self):
-        threading.Thread(target=self.send_bidrectional_packets).start()
+        threading.Thread(target=self.send_bidrectional_packets_run).start()
+        threading.Thread(target=self.find_neighbours_run).start()
 
-    def send_bidrectional_packets(self):
+    def send_bidrectional_packets_run(self):
         while not self.is_finished:
             for bidirectional_host_address in self.bidirectional_host_addresses:
-                hello_packet = self.get_hello_packet(bidirectional_host_address)
-                self.tools.send_udp_packet(bidirectional_host_address, hello_packet)
+                self.send_hello_packet(bidirectional_host_address)
+
             time.sleep(config.SEND_TIME)
+
+    def find_neighbours_run(self):
+        while not self.is_finished:
+            if len(self.bidirectional_host_addresses) < config.NUMBER_OF_NEIGHBOURS:
+                if len(self.unidirectional_received_host_addresses) == 0:
+                    self.send_find_to_random()
+                else:
+                    self.send_find_to_unidirectional_receiveds()
+
+            time.sleep(config.CHECK_NEIGHBOUR_TIME)
 
     def get_hello_packet(self, host_address):
         return P2PPacket(self.host_address, config.HELLO_MESSAGE_TYPE, 
             self.bidirectional_host_addresses, time.time(), self.hosts_last_receive_time[host_address])
+
+    def send_hello_packet(self, host_address):
+        hello_packet = self.get_hello_packet(host_address)
+        self.tools.send_udp_packet(host_address, hello_packet)
+
+    def send_find_to_random(self):
+        random_host_address = random.choice(tuple(config.HOST_ADDRESSES - self.bidirectional_host_addresses))
+        self.send_hello_packet(random_host_address)
+
+    def send_find_to_unidirectional_receiveds(self):
+        necessary_sends = config.NUMBER_OF_NEIGHBOURS - len(self.bidirectional_host_addresses)
+
+        for unidirectional_received_host_address in self.unidirectional_received_host_addresses:
+            if necessary_sends == 0:
+                break
+            self.send_hello_packet(unidirectional_received_host_address)
+            necessary_sends -= 1
+                    
--- config.py
+++ config.py
@@ -1,9 +1,11 @@
-HOST_ADDRESSES = [("localhost", 7032), 
+HOST_ADDRESSES = {("localhost", 7032), 
                   ("localhost", 7033), 
                   ("localhost", 7034), 
                   ("localhost", 7035), 
                   ("localhost", 7036), 
-                  ("localhost", 7037)]
+                  ("localhost", 7037)}
 HELLO_MESSAGE_TYPE = "Hello"
 UNDEFINED = -1
 SEND_TIME = 2
+CHECK_NEIGHBOUR_TIME = 1
+NUMBER_OF_NEIGHBOURS = 3
